6e65ed09eda8126c2d1365f5b04f21593b120cf8
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-22 11:32:46 -0600

    Implement constructor with radius validation in Circle
    Added constructor to Circle class to validate radius input.

--- app/src/main/java/org/example/Circle.java
+++ app/src/main/java/org/example/Circle.java
@@ -4,21 +4,23 @@ public class Circle extends Shape {
     private double radius;
     private final double newRadius;
 
-    public Circle() {
+    public Circle(int par) {
+        if ( radius <= 0) {
+            throw new IllegalArgumentException("Enter a positive integer.");
+        }
         this.newRadius = radius;
         this.radius = newRadius;
     }
 
     @Override
     public double getArea() {
-        System.out.println("Circle Area");
         return Math.PI * radius * radius;
     }
 
     @Override
     public double getPerimeter() {
-        System.out.println("Circle Perimeter");
         return 2 * Math.PI * radius;
     }
 
 }
+
