c24186de5d7e41dde88ac1a16495e4fe2cc8758c
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-22 12:12:26 -0600

    Implement Polygon interface in IsocelesRightTriangle
    Updated IsocelesRightTriangle to implement Polygon interface and added numberOfSides method.

--- app/src/main/java/org/example/IsocelesRightTriangle.java
+++ app/src/main/java/org/example/IsocelesRightTriangle.java
@@ -2,17 +2,25 @@ package org.example;
 
 //IsocelesRightTriangle Class for inheritance of the RightTriangle class behavior
 //By extending RightTriangle which also extends the initial Shape class
-public class IsocelesRightTriangle extends RightTriangle {
+//Also implements newly made Polygon with the function to combine classes to the interface
+public class IsocelesRightTriangle extends RightTriangle implements Polygon {
 
     //A Isocles Right Triangle can be hypotenuse
     //Where the legs are congruent in length and width
     public IsocelesRightTriangle(int par) {
-        super(par, par, par);  //IsocelesRightTriangle calls RightTriangle with the constructed super
+        super(par, par);  //IsocelesRightTriangle calls RightTriangle with the constructed super
     }
 
     @Override
     public String toString() {
         return String.format("IsocelesRightTriangle [par=%.2f]", base);
     }
+
+    @Override
+    public int numberOfSides() {
+        return 3;
+    }
+
 }
+
 
