8bf9bfcd2ef4863ef0d2738c7cfe816ea786f611
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-22 12:13:04 -0600

    Update RightTriangle.java

--- app/src/main/java/org/example/RightTriangle.java
+++ app/src/main/java/org/example/RightTriangle.java
@@ -1,10 +1,11 @@
 package org.example;
 
-public class RightTriangle extends Shape {
+//Also implements newly made Polygon with the function to combine classes to the interface
+public class RightTriangle extends Shape implements Polygon {
     protected final double base;
     protected double height;
 
-    public RightTriangle(int par, int par1, int par2) {
+    public RightTriangle(int par, int par1) {
         this.base = height;
         this.height = base;
     }
@@ -18,6 +19,11 @@ public class RightTriangle extends Shape {
     public double getPerimeter() {
         double hypotenuse = Math.sqrt(base * base + height * height);
         return base + height + hypotenuse;
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 3;
     }
 
 }
