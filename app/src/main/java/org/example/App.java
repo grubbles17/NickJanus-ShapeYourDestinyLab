512838b47ad3075e073896434f62395112b9f958
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-22 11:32:21 -0600

    Implement area and perimeter calculations in main
    Added area and perimeter calculations for Circle, Rectangle, Square, RightTriangle, and IsocelesRightTriangle.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,17 +5,50 @@ public class App {
 
     System.out.println("You can write a driver program for your code here!");
 
-  Circle myCircle = new Circle();
-  myCircle.getArea();
-  myCircle.getPerimeter();
 
-  Rectangle myRectangle = new Rectangle();
-  myRectangle.getArea();
-  myRectangle.getPerimeter();
+try {
+//For Circle Area/Perimeter
+  Circle myCircle = new Circle(3);
 
-  RightTriangle myRightTriangle = new RightTriangle();
-  myRightTriangle.getArea();
-  myRightTriangle.getPerimeter();
+  System.out.println(myCircle);
+  System.out.println("Circle Area: " + myCircle.getArea());
+  System.out.println("Circle Perimeter: " + myCircle.getPerimeter());
 
+  System.out.println(" ");
+//For Rectangle and Square
+  Rectangle myRectangle = new Rectangle(4, 8);
+  Square mySquare = new Square(6);
+
+//For Rectangle Area/Perimeter
+  System.out.println(myRectangle);
+  System.out.println("Rectangle Area: " + myRectangle.getArea());
+  System.out.println("Rectangle Perimeter: " + myRectangle.getPerimeter());
+
+  System.out.println(" ");
+//For Square Area/Perimeter
+  System.out.println(mySquare);
+  System.out.println("Square Area: " + myRectangle.getArea());
+  System.out.println("Square Perimeter: " + myRectangle.getPerimeter());
+
+  System.out.println(" ");
+//For RightTriangle and IsocelesRightTriangle
+  RightTriangle myRightTriangle = new RightTriangle(3, 4, 5);
+  IsocelesRightTriangle myIsocelesRightTriangle = new IsocelesRightTriangle(4);
+
+  System.out.println(" ");
+//For RightTriangle Area/Perimeter
+  System.out.println(myRightTriangle);
+  System.out.println("RightTriangle Area: " + myRightTriangle.getArea());
+  System.out.println("RightTriangle Perimeter: " + myRightTriangle.getPerimeter());
+
+//For IsocolesRightTriangle Area/Perimeter
+  System.out.println(myIsocelesRightTriangle);
+  System.out.println("IsocelesRightTriangle Area: " + myIsocelesRightTriangle.getArea());
+  System.out.println("IsocelesRightTriangle Perimeter: " + myIsocelesRightTriangle.getPerimeter());
+
+} catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+}
   }
 }
+
