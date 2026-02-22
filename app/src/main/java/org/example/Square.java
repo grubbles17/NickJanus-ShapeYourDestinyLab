b85bc7d02d58b5731724bfe8d7e47bb0eba5c8f1
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-22 12:13:42 -0600

    Implement Polygon interface in Square class

--- app/src/main/java/org/example/Square.java
+++ app/src/main/java/org/example/Square.java
@@ -2,7 +2,8 @@ package org.example;
 
 //Square Class for inheritance of the Rectangle class behavior
 //By extending Rectangle which also extends the initial Shape class
-public class Square extends Rectangle {
+//Also implements newly made Polygon with the function to combine classes to the interface
+public class Square extends Rectangle implements Polygon {
 
     //All squares have identical width/height
     public Square(double side) {
@@ -13,4 +14,11 @@ public class Square extends Rectangle {
     public String toString() {
         return String.format("Square [side=%.2f]", width);
     }
+
+     @Override
+    public int numberOfSides() {
+        return 4;
+    }
+    
 }
+
